9c912899fee201ea34d7207185624ab3ad1ad2b3
Axa Uribe <uri19002@byui.edu>
2023-05-12 08:51:23 -0600

    Almost done W03!

---
+++ Week03/Scripts/main.js
@@ -0,0 +1,7 @@
+menubutton = document.querySelector("#menu-button");
+
+menubutton.addEventListener("click", () => {
+  document.querySelector("#navbar").classList.toggle("active");
+  document.querySelector("#menu-open").classList.toggle("active");
+  document.querySelector("#menu-close").classList.toggle("active");
+});
