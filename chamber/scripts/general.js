1a4d3917609a5a9d0e0eada001a34fd5dd5d5535
Axa Uribe <uri19002@byui.edu>
2023-06-14 13:04:57 -0600

    Final_Eskeleton_Home_Page

--- chamber/scripts/general.js
+++ chamber/scripts/general.js
@@ -18,3 +18,7 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
   new Date()
 );
 document.querySelector(".p-nav-bar").textContent = fulldate;
+
+// if (today.getDay() == 1 || today.getDay() == 2) {
+//   document.querySelector(".meeting").style.display = "block";
+// }
