19670ecdc7005f09991a2428bed252e111e6bca0
Axa Uribe <uri19002@byui.edu>
2023-06-21 01:52:50 -0600

    W07-Activity

---
+++ chamber/scripts/last-visit.js
@@ -0,0 +1,15 @@
+const MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
+let hoy = new Date();
+let lastVisitString = localStorage.getItem("lastVisit");
+let visitspan = document.querySelector("#last-visit");
+
+if (lastVisitString == null) {
+  visitspan.textContent = "0";
+} else {
+  lastVisitDate = new Date(lastVisitString);
+  daysSinceLastVisit = Math.floor(
+    (hoy.getTime() - lastVisitDate.getTime()) / MILLIS_PER_DAY
+  );
+  visitspan.textContent = daysSinceLastVisit;
+}
+localStorage.setItem("lastVisit", hoy.toLocaleDateString());
